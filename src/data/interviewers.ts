46705d99d95b8be0f22d346ca2fe81de053765a8
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2025-08-24 22:34:55 -0400

    Add additional positions for mocks

--- src/data/interviewers.ts
+++ src/data/interviewers.ts
@@ -6,7 +6,47 @@ export const interviewers: InterviewCard[] = [
     position: 'Private Equity Associate',
     company: 'Ontario Teachers Pension Plan',
     tenure: 2,
-    signUpLink: 'https://cal.com/private-equity/30min',
+    signUpLink: 'https://cal.com/myfirstresume/pe-30min',
     thumbnail: '/logos/otpp.png'
+  },
+  {
+    id: '2',
+    position: 'Consulting Associate',
+    company: 'McKinsey / Bain / BCG',
+    tenure: 2,
+    signUpLink: 'https://cal.com/myfirstresume/mbb30min',
+    thumbnail: '/logos/bain.jpg'
+  },
+  {
+    id: '3',
+    position: 'Investment Banking Analyst',
+    company: 'Goldman Sachs / JPMorgan / Morgan Stanley',
+    tenure: 2,
+    signUpLink: 'https://cal.com/myfirstresume/ib30min',
+    thumbnail: '/logos/mstanley.png'
+  },
+  {
+    id: '4',
+    position: 'Big 4 Auditor',
+    company: 'PwC / EY / KPMG / Deloitte',
+    tenure: 3,
+    signUpLink: 'https://cal.com/myfirstresume/b430min',
+    thumbnail: '/logos/ey.png'
+  },
+  {
+    id: '5',
+    position: 'Software Engineer',
+    company: 'MANGA+',
+    tenure: 3,
+    signUpLink: 'https://cal.com/myfirstresume/swe30min',
+    thumbnail: '/logos/google.png'
+  },
+  {
+    id: '6',
+    position: 'Product Manager',
+    company: 'MANGA+',
+    tenure: 2,
+    signUpLink: 'https://cal.com/myfirstresume/pm30min',
+    thumbnail: '/logos/google.png'
   },
 ];
